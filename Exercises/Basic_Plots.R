ed94765f640c65a61b55b2e62c522c192f6e0b7a
IbisMalko <Ibis.Malko@gmail.com>
2020-03-15 11:57:04 -0700

    Initial Commit

--- Exercises/Basic_Plots.R
+++ Exercises/Basic_Plots.R
@@ -2,12 +2,15 @@
 library(dslabs)
 data("murders")
 
+library(dplyr)
+
 # a simple scatterplot of total murders versus population
 x <- murders$population /10^6
 y <- murders$total
 plot(x, y)
 
 # a histogram of murder rates
+murders <- mutate(murders, rate = murders$total/murders$population*100000)
 hist(murders$rate)
 
 # boxplots of murder rates by region
